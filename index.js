f3b48a0e024f4f6833103d47920442d93422a1a6
456leh <164193156+456leh@users.noreply.github.com>
2024-09-06 11:16:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const titulo = document.querySelector('.titulo');
+const imagemPerfil = document.querySelector('.image-perfil');
+const loginPerfil = document.querySelector('.login-perfil');
+const segPerfil = document.querySelector('.seg-perfil');
+const repPerfil = document.querySelector('.rep-perfil');
+const bioPerfil = document.querySelector('.bio')
+lehvenceslau
+const getInfo = async () =>{
+    const response = await fetch('https://api.instagram.com/users/lehvenceslau');
+    const data = await response.json();
+    console.log(data);
+    titulo.innerHTML = data.name;
+    repPerfil.innerHTML = data.public_repos + ' repositorio(s)';
+    imagemPerfil.setAttribute('src',data.avatar_url)
+    bioPerfil.innerHTML = data.bio
+    segPerfil.innerHTML = `Seguidores ${data.followers} e seguindo ${data.following} perfis`
+}
+
+getInfo()
